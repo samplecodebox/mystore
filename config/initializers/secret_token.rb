b7b6c7438d38f736f87d4712cebb07875042005b
= <=>
2012-03-09 16:47:53 -0800

    adding mystore project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = '12b50bd5f6ab68ed33583c5acac37c37769fff09bff096106ab581c8ecfc407e102bdaee73f6ca21e7a2d060667e5eb18386ea296fe9a5bc3e8de5f83d978d61'
