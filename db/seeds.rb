9e47a13edcc457eef8ce14afedd765974b512392
Riddhima Sejpal <riddhima.sejpal@gmail.com>
2012-03-10 17:28:10 -0800

    scaffolding product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create(:title => "Apple", :description => "%{ <p> My name is apple. I am red in color.
+ Very healty fruit</p> }", :image_url => "/images/apple.jpg", :price => "15.00")
+Product.create(:title => "Mango", :description => "%{ <p> My name is apple. I am yellow in color.
+ Very healty fruit</p> }", :image_url => "/images/apple.jpg", :price => "14.00")
+Product.create(:title => "Orange", :description => "%{ <p> My name is orange. I am orange in color.
+ Very healty fruit</p> }", :image_url => "/images/apple.jpg", :price => "12.50")
